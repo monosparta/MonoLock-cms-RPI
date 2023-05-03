2a0e9046953a1ae62829a660926bc29de322282c
jimchen5209 <jimchen5209@gmail.com>
2023-05-03 15:54:45 +0800

    feat: Enter offline mode when connection error

--- MonoLock.py
+++ MonoLock.py
@@ -19,6 +19,9 @@ class MonoLock:
         if (self.__sever_port != ""):
             self.__sever_port = ":" + self.__sever_port
 
+        if not os.path.exists('data/'):
+            os.mkdir('data/')
+
     def publish_error(self, id, error):
         client = mqtt.Client()
         client.username_pw_set(self.__mqtt_username, self.__mqtt_passsword)
@@ -28,15 +31,48 @@ class MonoLock:
         print(f"[MQTT] Sent ID: {id} Error: {error}")
 
     def get_id(self, card_number):
-        res = requests.post(
-            self.__sever_host + self.__sever_port + '/api/RPIunlock',
-            headers={'token': self.__token},
-            data={"cardId": card_number}
-        )
-        if (res.status_code >= 400):
-            print(f"[Request] Status: {res.status_code} Body: {json.loads(res.text)}")
-            print(f"[MonoLock] Could find locker id by {card_number}")
-            return
-        lock_id = res.text.replace('"', '')
+        try:
+            res = requests.post(
+                self.__sever_host + self.__sever_port + '/api/RPIunlock',
+                headers={'token': self.__token},
+                data={"cardId": card_number}
+            )
+            if (res.status_code >= 400):
+                print(f"[Request] Status: {res.status_code} Body: {json.loads(res.text)}")
+                print(f"[MonoLock] Could not find locker id by {card_number}")
+                return
+            lock_id = res.text.replace('"', '')
+        except requests.ConnectionError as e:
+            print(f"[Request] Connection error: {e}")
+            return self.__get_offline_id(card_number)
         print(f"[MonoLock] Found {card_number} -> {lock_id}")
         return lock_id
+
+    def __get_offline_id(self, card_number):
+        print("[MonoLock] Enter offline mode.")
+        if not os.path.exists('data/locker.json') and not os.path.exists('data/member.json'):
+            print("[MonoLock] No offline data found.")
+            return
+
+        with open('data/locker.json', 'r') as fs:
+            locker = json.load(fs)
+
+        with open('data/member.json', 'r') as fs:
+            member = json.load(fs)
+
+        if card_number == '000':
+            print("[MonoLock/Offline] Card read fail")
+            return
+
+        if card_number not in member:
+            print(f"[MonoLock/Offline] Could not find locker id by {card_number}")
+            return
+
+        locker_no = member[card_number]
+        print(f"[MonoLock/Offline] Found {card_number} -> No. {locker_no}")
+        if locker_no not in locker:
+            print(f"[MonoLock/Offline] {locker_no} does not have a locker id")
+            return
+        locker_id = locker[locker_no]
+        print(f"[MonoLock/Offline] Found No. {locker_no} -> {locker_id}")
+        return locker_id
